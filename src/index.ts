c34e65f2b29244b565d8851380c079aabb37648b
pstaal <peter.staal@gmail.com>
2021-08-09 15:41:26 +0200

    debugging

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import sharp from 'sharp';
+import path from 'path';
 
 const app = express();
 const port = 3000;
-const inputFile = '../images/full/fjord.jpg';
-const outputFile = '../images/resized/outputfile.jpg';
+const inputFile = path.resolve('images/full', "fjord.jpg");
+const outputFile = path.resolve("images/resized", "output.jpg");
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
