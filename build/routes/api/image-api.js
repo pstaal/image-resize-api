77dca49cca64d328cc26add6b89ee570c23faf64
pstaal <peter.staal@gmail.com>
2021-08-10 16:31:35 +0200

    debugging

--- build/routes/api/image-api.js
+++ build/routes/api/image-api.js
@@ -8,8 +8,8 @@ var resize_image_1 = __importDefault(require("../../utilities/resize-image"));
 var imageApi = express_1.default.Router();
 imageApi.get('/', function (req, res) {
     var filename = req.query.filename;
-    var width = req.query.width;
-    var height = req.query.height;
+    var width = parseInt(req.query.width);
+    var height = parseInt(req.query.height);
     res.sendFile(resize_image_1.default(filename, width, height));
 });
 exports.default = imageApi;
