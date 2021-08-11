6891db340d876b6b81a26fd10d9342cd89ef4f76
pstaal <peter.staal@gmail.com>
2021-08-11 09:54:47 +0200

    debugging

--- src/routes/api/image-api.ts
+++ src/routes/api/image-api.ts
@@ -1,14 +1,29 @@
 import express from 'express';
 import resizeImage from '../../utilities/resize-image';
+import path from 'path';
+import fs from 'fs';
 const imageApi = express.Router();
 
-imageApi.get('/', (req, res) => {
+
+imageApi.get('/', async (req, res) => {
 
   let filename: string = req.query.filename;
   let width: number = parseInt(req.query.width);
   let height: number = parseInt(req.query.height);
 
-  res.sendFile(resizeImage(filename, width, height));
+  // create path for input file
+  let inputFile = path.resolve(__dirname, `images/full/${filename}.jpg`);
+  
+  // create path for output file
+  let outputFile = path.resolve(__dirname, `images/thumb/${width}-${height}-${filename}.jpg`);
+
+  // check if the file already exists
+  if (fs.existsSync(outputFile)) {
+    res.sendFile(outputFile);
+  } else {
+    outputFile = await resizeImage(inputFile, outputFile, width, height);
+    res.sendFile(outputFile);
+  }
 
 });
 
--- src/utilities/resize-image.ts
+++ src/utilities/resize-image.ts
@@ -1,34 +1,19 @@
 import sharp from 'sharp';
-import path from 'path';
-import fs from 'fs';
 
 
 
-const resizeImage = (filename: string, width: number, height: number): string => {
+const resizeImage = async (inputfile: string, outputfile: string, width: number, height: number): string => {
 
-let inputFile;
 try {
-inputFile = path.resolve('images/full', `${filename}.jpg`);
-} 
-catch(error) {
-  console.log(error.message);
-}
-
-
-// create path for output file
-const outputFile = path.resolve("images/thumb", `${width}-${height}-${filename}.jpg`);
-
-if (fs.existsSync(outputFile)) {
-  return outputFile;
-}
-else {
-sharp(inputFile)
+await sharp(inputfile)
 .resize(width, height)
-.toFile(outputFile).then(info => { console.log(info) })
+.toFile(outputfile).then(info => { console.log(info) })
 .catch((error) => console.log(error.message))
-};
+} catch(error) {
+  console.log(error.message)
+}
 
-return outputFile;
+return outputfile;
 
 };
 
