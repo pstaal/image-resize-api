246fab1318e76481b3ef5abe044d7f6f353248a2
pstaal <peter.staal@gmail.com>
2021-08-11 13:59:41 +0200

    add lint configuration

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ import routes from './routes/index';
 const app = express();
 const port = 3000;
 
-
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
 
 app.use('/api', routes);
-
 
 export default app;
